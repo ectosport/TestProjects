fa86431df88860912996117e67749eab570ee7bb
ectosport <reidlee@yahoo.com>
2015-02-20 09:32:32 -0600

    Added BowlingGame to TestProjects repo

---
+++ BowlingGame/BowlingGame/BowlingGame.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGame
+{
+    public class BowlingGame
+    {
+        private readonly List<int> rolls = new List<int>();
+        private int currentFrame = 1;
+        private int currentRollOfFrame = 1;
+
+        static public string ExceptionMoreThan10PinsSingleRollMessage = "Cannot hit more than 10 pins in a single roll.";
+        static public string ExceptionMoreThan10PinsTwoRollsMessage = "Pins hit in current frame cannot exceed 10.";
+
+        public BowlingGame()
+        {
+                
+        }
+
+        public void Roll(int pinsHit)
+        {
+            rolls.Add(pinsHit);
+
+            // validate "rules"
+            if (pinsHit > 10)
+            {
+                throw new ArgumentException(ExceptionMoreThan10PinsSingleRollMessage);
+            }
+            else if (currentRollOfFrame == 2 && (pinsHit + rolls[rolls.Count - 1]) > 10)
+            {
+                throw new ArgumentException(ExceptionMoreThan10PinsTwoRollsMessage);
+            }
+
+            if (pinsHit == 10)
+            {
+                ++currentFrame;
+                currentRollOfFrame = 1;
+            }
+            else if (currentRollOfFrame == 2)
+            {
+                ++currentFrame;
+                currentRollOfFrame = 1;
+            }
+            else
+            {
+                ++currentRollOfFrame;
+            }
+        }
+
+        public void Reset()
+        {
+            rolls.Clear();
+        }
+
+        public int Score
+        {
+            get
+            {
+                int score = 0;
+                int frame = 0;
+
+                for (int rollIndex = 0; rollIndex < rolls.Count && frame < 10; ++frame)
+                {
+                    if (rolls[rollIndex] == 10) // strike
+                    {
+                        score += 10 + rolls[rollIndex + 1] + rolls[rollIndex + 2];
+                        ++rollIndex;
+                    }
+                    else if (rolls[rollIndex] + rolls[rollIndex + 1] == 10) // spare
+                    {
+                        score += 10 + rolls[rollIndex + 2];
+                        rollIndex += 2;
+                    }
+                    else
+                    {
+                        score += rolls[rollIndex] + rolls[rollIndex + 1];
+                        rollIndex += 2;
+                    }
+                }
+
+                return score;
+            } 
+            
+        }
+    }
+}
---
+++ BowlingGame/BowlingGameTests/BowlingGameTests.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BowlingGame;
+
+namespace BowlingGameTests
+{
+    [TestClass]
+    public class BowlingGameTests
+    {
+        
+
+        [TestMethod]
+        public void TestAllGutters()
+        {
+            BowlingGame.BowlingGame game = new BowlingGame.BowlingGame();
+
+            RollBowlingBall(game, 0, 20);
+
+            Assert.AreEqual(game.Score, 0);
+        }
+
+        [TestMethod]
+        public void TestAllSinglePins()
+        {
+            BowlingGame.BowlingGame game = new BowlingGame.BowlingGame();
+
+            RollBowlingBall(game, 1, 20);
+
+            Assert.AreEqual(game.Score, 20);
+        }
+
+        [TestMethod]
+        public void TestSingleSpare()
+        {
+            BowlingGame.BowlingGame game = new BowlingGame.BowlingGame();
+
+            game.Roll(6);
+            game.Roll(4);
+            game.Roll(7);
+            RollBowlingBall(game, 0, 17);
+
+            Assert.AreEqual(game.Score, 24);
+        }
+
+        [TestMethod]
+        public void TestSingleStrike()
+        {
+            BowlingGame.BowlingGame game = new BowlingGame.BowlingGame();
+
+            game.Roll(10);
+            game.Roll(4);
+            game.Roll(5);
+            RollBowlingBall(game, 0, 16);
+
+            Assert.AreEqual(game.Score, 28);
+        }
+
+        [TestMethod]
+        public void TestTwoStrikes()
+        {
+            BowlingGame.BowlingGame game = new BowlingGame.BowlingGame();
+
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(5);
+            game.Roll(4);
+            RollBowlingBall(game, 0, 14);
+
+            Assert.AreEqual(game.Score, 53);
+        }
+
+        [TestMethod]
+        public void TestPerfectGame()
+        {
+            BowlingGame.BowlingGame game = new BowlingGame.BowlingGame();
+
+            RollBowlingBall(game, 10, 12);
+
+            Assert.AreEqual(game.Score, 300);
+        }
+
+        [TestMethod]
+        public void TestHittingMoreThan10Pins()
+        {
+            BowlingGame.BowlingGame game = new BowlingGame.BowlingGame();
+
+            try
+            {
+                game.Roll(11);
+            }
+            catch (ArgumentException e)
+            {
+                StringAssert.Contains(e.Message, BowlingGame.BowlingGame.ExceptionMoreThan10PinsSingleRollMessage);
+                return;
+            }
+
+            Assert.Fail("Expected exception for hitting more than 10 pins in a single roll.");
+        }
+
+        [TestMethod]
+        public void TestHittingMoreThan10PinsTwoRolls()
+        {
+            BowlingGame.BowlingGame game = new BowlingGame.BowlingGame();
+
+            try
+            {
+                game.Roll(5);
+                game.Roll(6);
+            }
+            catch (ArgumentException e)
+            {
+                StringAssert.Contains(e.Message, BowlingGame.BowlingGame.ExceptionMoreThan10PinsTwoRollsMessage);
+                return;
+            }
+
+            Assert.Fail("Expected exception for hitting more than 10 pins in two rolls.");
+        }
+
+        private static void RollBowlingBall(BowlingGame.BowlingGame game, int pinsHit, int numberOfRolls)
+        {
+            for (int i = 0; i < numberOfRolls; ++i)
+            {
+                game.Roll(pinsHit);
+            }
+        }
+    }
+}
+
