3ebf14e6a805dfaa3cd532f1ff49663b3eba64e8
Reid Lee <Reid.Lee@cirrus.com>
2019-01-28 09:30:23 -0600

    Reformatted document

--- GrepperWPF/GrepperWPF/CommandHandler.cs
+++ GrepperWPF/GrepperWPF/CommandHandler.cs
@@ -3,25 +3,25 @@ using System.Windows.Input;
 
 namespace SimpleSearch
 {
-   internal class CommandHandler : ICommand
-   {
-      private Action<object> _execute;
-      private Func<bool> _canExecute;
+    internal class CommandHandler : ICommand
+    {
+        private Action<object> _execute;
+        private Func<bool> _canExecute;
 
-      public CommandHandler(Action<object> execute, Func<bool> canExecute)
-      {
-         _execute = execute;
-         _canExecute = canExecute;
-      }
+        public CommandHandler(Action<object> execute, Func<bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
 
-      public event EventHandler CanExecuteChanged
-      {
-         add { CommandManager.RequerySuggested += value; }
-         remove { CommandManager.RequerySuggested -= value; }
-      }
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
 
-      public void Execute(object parameter) => _execute(parameter);
+        public void Execute(object parameter) => _execute(parameter);
 
-      public bool CanExecute(object parameter) => _canExecute();
-   }
+        public bool CanExecute(object parameter) => _canExecute();
+    }
 }
